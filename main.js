ac92f389364ee4de03859e6e9d18677606c9e72b
Alex.Liu <larch.liu@gmail.com>
2023-03-13 20:00:41 +0800

    init

---
+++ main.js
@@ -0,0 +1,91 @@
+import './style.css';
+import { WebContainer } from '@webcontainer/api';
+import { files } from './files';
+import { Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
+
+/** @type {import('@webcontainer/api').WebContainer}  */
+let webcontainerInstance;
+
+window.addEventListener('load', async () => {
+  textareaEl.value = files['slides.md'].file.contents;
+  textareaEl.addEventListener('input', (e) => {
+    writeIndexJS(e.currentTarget.value);
+  });
+
+  const terminal = new Terminal({
+    convertEol: true,
+  });
+  terminal.open(terminalEl);
+
+  // Call only once
+  webcontainerInstance = await WebContainer.boot();
+  await webcontainerInstance.mount(files);
+
+  const exitCode = await installDependencies(terminal);
+  if (exitCode !== 0) {
+    throw new Error('Installation failed');
+  }
+
+  startDevServer(terminal);
+});
+
+async function installDependencies(terminal) {
+  // Install dependencies
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  installProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        terminal.write(data);
+      },
+    })
+  );
+  // Wait for install command to exit
+  return installProcess.exit;
+}
+
+async function startDevServer(terminal) {
+  // Run `npm run start` to start the Express app
+  const serverProcess = await webcontainerInstance.spawn('npm', ['run', 'dev']);
+
+  serverProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        terminal.write(data);
+      },
+    })
+  );
+
+  // Wait for `server-ready` event
+  webcontainerInstance.on('server-ready', (port, url) => {
+    iframeEl.src = url;
+  });
+}
+
+/**
+ * @param {string} content
+ */
+
+async function writeIndexJS(content) {
+  await webcontainerInstance.fs.writeFile('/slides.md', content);
+}
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="editor">
+      <textarea>I am a textarea</textarea>
+    </div>
+    <div class="preview">
+      <iframe src="loading.html"></iframe>
+    </div>
+    <div class="terminal"></div>
+  </div>
+`;
+
+/** @type {HTMLIFrameElement | null} */
+const iframeEl = document.querySelector('iframe');
+
+/** @type {HTMLTextAreaElement | null} */
+const textareaEl = document.querySelector('textarea');
+
+const terminalEl = document.querySelector('.terminal');
